e96e97234e2f734a49cf7234f184d412ce107c10
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-03-03 23:12:36 -0500

    ported fourier parameterization code

--- fourier.py
+++ fourier.py
@@ -1,5 +1,5 @@
 """
-This module is based off of the `spatial` module in lucid.
+NOTE: This module is a modified version of the `spatial` module in lucid so that it works with pytorch tensors.
 """
 
 import numpy as np
@@ -13,25 +13,18 @@ def rfft2d_freqs(h, w):
     return np.sqrt(fx * fx + fy * fy)
 
 
-def fft_image(shape):
-    sd = 0.01
+def fourier_parameterized_image(shape, sd=0.01):
     batch, h, w, ch = shape
     freqs = rfft2d_freqs(h, w)
     init_val_size = (2, batch, ch) + freqs.shape
-    init_val_size = (batch, ch) + freqs.shape + (2,)
-    
-    init_val = np.random.normal(size=init_val_size, scale=sd).astype(np.float32)
-    init_val = torch.from_numpy(init_val)
-    
+    init_val = torch.empty(init_val_size).normal_(std=sd)
+
     scale = 1.0 / np.maximum(freqs, 1.0 / max(w, h))
-#     scale *= np.sqrt(w * h)
     scale = torch.from_numpy(scale).float()
-    init_val = scale * init_val
-    
+    init_val *= scale
+
     init_val = init_val.permute(1, 2, 3, 4, 0)
-    
-    image_t = torch.irfft(init_val, signal_ndim=2, normalized=True)
-    image_t = image_t[:batch, :ch, :h, :w]
-    image_t = image_t / 4.0
-    
-    return image_t
+    image = torch.irfft(init_val, signal_ndim=2, normalized=True, onesided=True, signal_sizes=(h, w))
+    image = image / 4.0  # the four here is a magic number defined in the lucid implementation
+
+    return image
