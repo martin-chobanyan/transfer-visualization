784ccbbab449517c85787053a4612a3ffbbf6bac
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-03-03 23:40:15 -0500

    added documentation

--- fourier.py
+++ fourier.py
@@ -13,11 +13,31 @@ def rfft2d_freqs(h, w):
     return np.sqrt(fx * fx + fy * fy)
 
 
-def fourier_parameterized_image(shape, sd=0.01):
+def fourier_parameterized_image(shape=(1, 224, 224, 3), std_dev=0.01):
+    """Create the initial image using the Fourier Transform
+
+    Initializing the image with a Gaussian distribution in the pixel space will result in a lot of high frequency
+    artifacts. Instead, this function initializes the Gaussian in the Fourier space and then uses the inverse Fourier
+    Transform to bring the image back to the pixel space.
+
+    Parameters
+    ----------
+    shape: tuple[int], optional
+        The desired image shape. The format is as follows: (batch_size, height, width, num_channels).
+        The default shape is (1, 224, 224, 3), resulting in a single 3-channel image.
+    std_dev: float, optional
+        The standard deviation used by the normal distribution when initializing the Fourier spectrum.
+        Lower values will result in lower frequencies (the image will look more gray).
+
+    Returns
+    -------
+    torch.FloatTensor
+        A pytorch tensor containing the Fourier parameterized images.
+    """
     batch, h, w, ch = shape
     freqs = rfft2d_freqs(h, w)
     init_val_size = (2, batch, ch) + freqs.shape
-    init_val = torch.empty(init_val_size).normal_(std=sd)
+    init_val = torch.empty(init_val_size).normal_(std=std_dev)
 
     scale = 1.0 / np.maximum(freqs, 1.0 / max(w, h))
     scale = torch.from_numpy(scale).float()
