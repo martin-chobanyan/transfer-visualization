238227d237acf92a2b16845822d60ac9ac955261
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-03-05 23:27:04 -0500

    added docs

--- transforms.py
+++ transforms.py
@@ -4,9 +4,20 @@ from kornia.geometry import rotate
 
 
 class RotateTensor:
+    """Rotate a pytorch tensor
+
+    Rotates a pytorch tensor representing image along its center.
+    When calling this object, the input must be a tensor with shape (batch, channel, height, width).
+
+    Parameters
+    ----------
+    angles: list[float]
+        A list of rotation angles to sample from
+    """
     def __init__(self, angles):
         self.angles = angles
 
     def __call__(self, x):
         angle = random.choice(self.angles)
         return rotate(x, torch.tensor(angle))
+
