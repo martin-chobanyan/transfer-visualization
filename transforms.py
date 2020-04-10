9f3437a6fe8cf428eb132d0fade874d651f06ebe
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-04-09 23:18:28 -0400

    added Normalize

--- transforms.py
+++ transforms.py
@@ -5,7 +5,7 @@ This module provides helper objects for transforming image tensors using the kor
 import random
 import torch
 from torch.nn import ReflectionPad2d
-from torchvision.transforms import Compose
+from torchvision.transforms import Compose, Normalize
 from kornia.geometry import rotate, scale, translate
 from utils import torch_uniform
 
@@ -88,10 +88,15 @@ class TranslateTensor:
         return translate(x, shifts)
 
 
+IMAGENET_MEANS = [0.485, 0.456, 0.406]
+IMAGENET_STDEVS = [0.229, 0.224, 0.225]
+
+
 # the default set of transformations matches those as in the "Feature Visualization" distill article
 # https://distill.pub/2017/feature-visualization/
 def get_default_transforms(device):
     return Compose([
+        # Normalize(mean=IMAGENET_MEANS, std=IMAGENET_STDEVS),
         ReflectionPad2d(16),
         TranslateTensor(max_shift=16, device=device),
         ScaleTensor(scale_factors=[1, 0.975, 1.025, 0.95, 1.05], device=device),
