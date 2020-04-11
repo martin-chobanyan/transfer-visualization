dd1584c70ba0245bf2d8a24676c899f3bcb45f26
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-04-11 00:44:50 -0400

    added empty script for finetuning the dog breed dataset + dataset class

---
+++ transfer/finetune_dog_classifier.py
@@ -0,0 +1,41 @@
+import os
+
+from pandas import read_csv
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import Dataset
+
+class DogBreedDataset(Dataset):
+    def __init__(self, root_dir, transforms=None):
+        super().__init__()
+        self.root_dir = root_dir
+        self.transforms = transforms
+        self.img_dir = os.path.join(root_dir, 'train')
+
+        dog_df = read_csv(os.path.join(root_dir, 'labels.csv'))
+        self.dog_ids = dog_df['id'].values
+
+        self.label_encoder = LabelEncoder()
+        self.labels = self.label_encoder.fit_transform(dog_df['breed'].values)
+
+    @property
+    def breeds(self):
+        return self.label_encoder.classes_
+
+    def __getitem__(self, item):
+        image = Image.open(os.path.join(self.img_dir, f'{self.dog_ids[item]}.jpg'))
+        label = self.labels[item]
+        if self.transforms is not None:
+            image = self.transforms(image)
+        return image, label
+
+    def __len__(self):
+        return len(self.dog_ids)
+
+if __name__ == '__main__':
+    root_dir = '/home/mchobanyan/data/kaggle/dog_breeds/'
+    dataset = DogBreedDataset(root_dir)
+    img, label = dataset[200]
+    img.show()
+    print(label)
+    print(list(zip(range(len(dataset.breeds)), dataset.breeds)))
