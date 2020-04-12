f6b5f57fad670fb0ac90fa285a32d05611435370
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-04-11 22:38:26 -0400

    added training utilities files

---
+++ transfer/train_utils.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn.functional as F
+
+def accuracy(model_out, true_labels):
+    """Calculate the accuracy of a batch of predictions
+
+    Parameters
+    ----------
+    model_out: torch.FloatTensor
+        The output of the emotion classifier with shape [batch size, num emotions]
+    true_labels: torch.LongTensor
+        The true encoded emotion labels aligned with the model's output
+
+    Returns
+    -------
+    float
+    """
+    pred = torch.argmax(F.softmax(model_out, dim=1), dim=1)
+    acc = (pred == true_labels.squeeze())
+    return float(acc.sum()) / acc.size(0)
+
+
+def train_epoch(model, dataloader, criterion, optimizer, device):
+    """Train the model for an epoch
+
+    Parameters
+    ----------
+    model: nn.Module
+    dataloader: DataLoader
+    criterion: callable loss function
+    optimizer: pytorch optimizer
+    device: torch.device
+
+    Returns
+    -------
+    float
+        The average loss
+    """
+    avg_loss = []
+    model.train()
+    for batch_image, batch_label in dataloader:
+        batch_image = batch_image.to(device)
+        batch_label = batch_label.to(device)
+        optimizer.zero_grad()
+        output = model(batch_image)
+        loss = criterion(output, batch_label)
+        loss.backward()
+        optimizer.step()
+        avg_loss.append(loss.item())
+    return sum(avg_loss) / len(avg_loss)
+
+
+def test_epoch(model, dataloader, criterion, device):
+    """Run the model for a validation epoch
+
+    Parameters
+    ----------
+    model: nn.Module
+    dataloader: DataLoader
+    criterion: callable loss function
+    device: torch.device
+
+    Returns
+    -------
+    float, float
+        The average loss and the average accuracy
+    """
+    avg_loss = []
+    avg_acc = []
+    model.eval()
+    with torch.no_grad():
+        for batch_image, batch_label in dataloader:
+            batch_image = batch_image.to(device)
+            batch_label = batch_label.to(device)
+            output = model(batch_image)
+            loss = criterion(output, batch_label)
+            acc = accuracy(output, batch_label)
+            avg_loss.append(loss.item())
+            avg_acc.append(acc)
+    return sum(avg_loss) / len(avg_loss), sum(avg_acc) / len(avg_acc)
+
+
+def checkpoint(model, filepath):
+    """Save the state of the model
+
+    To restore the model do the following:
+    >> the_model = TheModelClass(*args, **kwargs)
+    >> the_model.load_state_dict(torch.load(PATH))
+
+    Parameters
+    ----------
+    model: nn.Module
+        The pytorch model to be saved
+    filepath: str
+        The filepath of the pickle
+    """
+    torch.save(model.state_dict(), filepath)
