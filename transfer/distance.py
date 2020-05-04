68a2f4a29b35c06e2352dac859884f096593c9ab
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-05-04 00:39:09 -0400

    finalized gram matrix code

--- transfer/distance.py
+++ transfer/distance.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Module, Sequential
+from torch.nn import Module, ModuleList, Sequential
 
 
 def calc_gram_matrix(features):
@@ -20,23 +20,49 @@ def calc_gram_matrix(features):
     return gram
 
 
-class GramMatrixDistance(Module):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-
-    def forward(self, img1, img2):
-        if img1.size() != img2.size():
-            raise ValueError('Input images to GramMatrixLoss must have the same shape!')
-
-        features1 = self.model(img1)
-        features2 = self.model(img2)
-        b, c, h, w = features1.size()
-
+class GramMatrixLoss(Module):
+    def forward(self, features1, features2):
         gram1 = calc_gram_matrix(features1)
         gram2 = calc_gram_matrix(features2)
 
+        b, c, h, w = features1.size()
         scale = 1 / (4 * (c ** 2) * ((h * w) ** 2))
         gram_diff = (gram1 - gram2) ** 2
         gram_diff = gram_diff.view(b, -1)
         return scale * torch.sum(gram_diff, dim=1)
+
+
+class GramMatrixDistance(Module):
+    def __init__(self, full_model, target_layers=None):
+        super().__init__()
+        self.target_layers = target_layers
+        if self.target_layers is None:
+            self.target_layers = ['layer1', 'layer2', 'layer3', 'layer4']
+        self.layers = ModuleList(self.group_model_layers(full_model))
+
+    def group_model_layers(self, full_model):
+        final_layers = []
+        layer_bank = []
+        for name, child in full_model.named_children():
+            layer_bank.append(child)
+            if name in self.target_layers:
+                final_layers.append(Sequential(*layer_bank))
+                final_layers.append(GramMatrixLoss())
+                layer_bank.clear()
+        return final_layers
+
+    def forward(self, img1, img2):
+        if img1.size() != img2.size():
+            raise ValueError('Input images to GramMatrixLoss must have the same shape!')
+        features1 = img1
+        features2 = img2
+
+        losses = []
+        for layer in self.layers:
+            if isinstance(layer, GramMatrixLoss):
+                losses.append(layer(features1, features2))
+            else:
+                features1 = layer(features1)
+                features2 = layer(features2)
+        losses = torch.stack(losses).mean(dim=0)
+        return losses
