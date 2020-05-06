73b761434029154610fb69dc5da71f36e3a0677b
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-05-05 23:33:29 -0400

    moved default model loader functions to train_utils, created empty car script

---
+++ transfer/finetune_car_classifier.py
@@ -0,0 +1,29 @@
+import os
+
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader, random_split
+from torchvision.transforms import Compose, Normalize, RandomCrop, RandomHorizontalFlip, RandomRotation, ToTensor
+from tqdm import tqdm
+
+from feature_vis.transforms import IMAGENET_MEANS, IMAGENET_STDEVS
+from dataset import DogBreedDataset
+from train_utils import *
+
+# define the constants
+IMAGE_SHAPE = (400, 400)
+P_TRAIN = 0.8
+BATCH_SIZE = 100
+LEARNING_RATE = 0.00001
+NUM_EPOCHS = 60
+
+if __name__ == '__main__':
+    # define the dog breed dataset
+    root_dir = '/home/mchobanyan/data/kaggle/dog_breeds/'
+    transforms = Compose([
+        RandomCrop(IMAGE_SHAPE, pad_if_needed=True),
+        RandomHorizontalFlip(),
+        RandomRotation(30),
+        ToTensor(),
+        Normalize(IMAGENET_MEANS, IMAGENET_STDEVS)
+    ])
--- transfer/finetune_dog_classifier.py
+++ transfer/finetune_dog_classifier.py
@@ -3,14 +3,12 @@ import os
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader, random_split
-from torchvision.models import resnet50
 from torchvision.transforms import Compose, Normalize, RandomCrop, RandomHorizontalFlip, RandomRotation, ToTensor
 from tqdm import tqdm
 
 from feature_vis.transforms import IMAGENET_MEANS, IMAGENET_STDEVS
-from feature_vis.utils import freeze_parameters, unfreeze_parameters
 from dataset import DogBreedDataset
-from train_utils import checkpoint, create_folder, get_device, train_epoch, test_epoch, TrainingLogger
+from train_utils import *
 
 # define the constants
 IMAGE_SHAPE = (400, 400)
@@ -18,42 +16,6 @@ P_TRAIN = 0.8
 BATCH_SIZE = 100
 LEARNING_RATE = 0.00001
 NUM_EPOCHS = 60
-
-
-def load_resnet50_layer3_bottleneck5(num_classes):
-    """
-    Loads a pretrained resnet-50, swaps the fully connected layer,
-    and freezes all parameters except for those in layer3.bottleneck5, layer4 and the fully connected layer.
-    """
-    resnet_model = resnet50(pretrained=True)
-    resnet_model = freeze_parameters(resnet_model)
-
-    unfreeze_parameters(resnet_model.layer3[5])
-    unfreeze_parameters(resnet_model.layer4)
-
-    fc_input_dim = resnet_model.fc.in_features
-    new_fc = nn.Linear(fc_input_dim, num_classes)
-    resnet_model.fc = new_fc
-
-    return resnet_model
-
-
-def load_resnet50_layer4(num_classes):
-    """
-    Loads a pretrained resnet-50, swaps the fully connected layer,
-    and freezes all parameters except for those in layer4 and the fully connected layer.
-    """
-    resnet_model = resnet50(pretrained=True)
-    resnet_model = freeze_parameters(resnet_model)
-
-    unfreeze_parameters(resnet_model.layer4)
-
-    fc_input_dim = resnet_model.fc.in_features
-    new_fc = nn.Linear(fc_input_dim, num_classes)
-    resnet_model.fc = new_fc
-
-    return resnet_model
-
 
 if __name__ == '__main__':
     # define the dog breed dataset
