cf5d846b84890cb09103e534963ffecfc6dc50f9
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-05-23 16:14:04 -0400

    added run for both dogs and cars in compare_features

--- transfer/compare_features.py
+++ transfer/compare_features.py
@@ -21,9 +21,9 @@ LAYERS = ['layer3-bottleneck5', 'layer4-bottleneck0', 'layer4-bottleneck1', 'lay
 DEVICE = get_device()
 
 
-def compare_images(image_pairs, gram_fn, output_path, device, transforms):
+def compare_images(image_pairs, gram_fn, output_path, device, transforms, title=''):
     logger = Logger(output_path, header=['channel', 'gram_avg'] + [f'gram_layer{i}' for i in range(1, 5)])
-    for channel_idx in tqdm(range(len(image_pairs))):
+    for channel_idx in tqdm(range(len(image_pairs)), desc=title):
         img1, img2 = image_pairs[channel_idx]
         img1 = transforms(img1).unsqueeze(0).to(device)
         img2 = transforms(img2).unsqueeze(0).to(device)
@@ -36,8 +36,8 @@ def compare_images(image_pairs, gram_fn, output_path, device, transforms):
         logger.add_entry(*([channel_idx, gram_avg] + gram_distances))
 
 
-def main():
-    create_folder(OUTPUT_DIR)
+def run(imagenet_dir, target_dir, output_dir):
+    create_folder(output_dir)
     image_transforms = Compose([ToTensor(), ImagenetNorm()])
 
     model = resnet50(pretrained=True)
@@ -47,13 +47,18 @@ def main():
 
     # compare resnet50 pre-trained on ImageNet vs fine-tuned on the dog breed dataset
     for layer in LAYERS:
-        img_dir1 = os.path.join(IMAGENET_DIR, 'features', layer)
-        img_dir2 = os.path.join(DOG_DIR, 'features', layer)
+        img_dir1 = os.path.join(imagenet_dir, 'features', layer)
+        img_dir2 = os.path.join(target_dir, 'features', layer)
         image_pairs = ImagePairs(img_dir1, img_dir2)
 
-        output_path = os.path.join(OUTPUT_DIR, f'imagenet-vs-dogs-{layer}.csv')
-        compare_images(image_pairs, gram_distance, output_path, DEVICE, image_transforms)
+        output_path = os.path.join(output_dir, f'imagenet-vs-dogs-{layer}.csv')
+        compare_images(image_pairs, gram_distance, output_path, DEVICE, image_transforms, layer)
 
 
 if __name__ == '__main__':
-    main()
+    print('Comparing ImageNet vs Dog Breeds feature visualizations')
+    # run(IMAGENET_DIR, DOG_DIR, os.path.join(OUTPUT_DIR, 'dogs'))
+    print()
+
+    print('Comparing ImageNet vs Car Models feature visualizations')
+    run(IMAGENET_DIR, CAR_DIR, os.path.join(OUTPUT_DIR, 'cars'))
