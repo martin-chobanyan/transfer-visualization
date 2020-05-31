0d5e37fa6400ecc2fc9679a85e3be6c110ab85d9
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-05-31 12:11:31 -0400

    updated compare_features to use new CosineSimResnet50

--- transfer/compare_features.py
+++ transfer/compare_features.py
@@ -2,11 +2,14 @@ import os
 from statistics import mean
 
 from torch import no_grad
+from torch.nn import Sequential
+from torch.nn.functional import cosine_similarity
+from torchvision.models import resnet50
 from torchvision.transforms import Compose, ToTensor
 from tqdm import tqdm
 
 from dataset import ImagePairs
-from distance import GramDistanceResnet50
+from distance import GramDistanceResnet50, CosineSimResnet50
 from train_utils import create_folder, get_device, Logger
 from feature_vis.transforms import ImagenetNorm
 
@@ -15,7 +18,7 @@ ROOT_DIR = '/home/mchobanyan/data/research/transfer/vis/'
 IMAGENET_DIR = os.path.join(ROOT_DIR, 'pretrained-resnet50')
 DOG_DIR = os.path.join(ROOT_DIR, 'finetune-dog-resnet50')
 CAR_DIR = os.path.join(ROOT_DIR, 'finetune-car-resnet50')
-OUTPUT_DIR = os.path.join(ROOT_DIR, 'comparisons')
+OUTPUT_DIR = os.path.join(ROOT_DIR, 'emb-comparisons')
 LAYERS = ['layer3-bottleneck5', 'layer4-bottleneck0', 'layer4-bottleneck1', 'layer4-bottleneck2']
 DEVICE = get_device()
 
@@ -52,12 +55,62 @@ def run(gram_fn, imagenet_dir, target_dir, output_dir):
         compare_images(image_pairs, gram_fn, output_path, DEVICE, image_transforms, layer)
 
 
+def compare_images_with_cosine_sim(image_pairs, model, output_path, device, transforms, title=''):
+    """
+    Parameters
+    ----------
+    image_pairs: ImagePairs
+    model: CosineSimResnet50
+    output_path: str
+    device: torch.device
+    transforms: callable
+    title: str
+    """
+    # set up the logger
+    header = ['channel'] + [f'cos_sim_layer{i}' for i in range(1, model.num_emb_layers + 1)]
+    logger = Logger(output_path, header=header)
+
+    for channel_idx in tqdm(range(len(image_pairs)), desc=title):
+        img1, img2 = image_pairs[channel_idx]
+        img1 = transforms(img1).unsqueeze(0).to(device)
+        img2 = transforms(img2).unsqueeze(0).to(device)
+
+        # calculate the cosine similarities
+        with no_grad():
+            cosine_sims = model(img1, img2).squeeze().tolist()
+        logger.add_entry(channel_idx, *cosine_sims)
+
+
+def run_cosine_sim(model, imagenet_dir, target_dir, output_dir):
+    create_folder(output_dir)
+    image_transforms = Compose([ToTensor(), ImagenetNorm()])
+
+    # compare model pre-trained on ImageNet vs fine-tuned on the dog breed dataset
+    for layer in LAYERS:
+        img_dir1 = os.path.join(imagenet_dir, 'features', layer)
+        img_dir2 = os.path.join(target_dir, 'features', layer)
+        image_pairs = ImagePairs(img_dir1, img_dir2)
+
+        output_path = os.path.join(output_dir, f'resnet50-cosine-sim-{layer}.csv')
+        compare_images_with_cosine_sim(image_pairs, model, output_path, DEVICE, image_transforms, layer)
+
+
 if __name__ == '__main__':
-    gram_distance_fn = GramDistanceResnet50().to(DEVICE)
+    # gram_distance_fn = GramDistanceResnet50().to(DEVICE)
+    #
+    # print('Comparing ImageNet vs Dog Breeds feature visualizations')
+    # run(gram_distance_fn, IMAGENET_DIR, DOG_DIR, os.path.join(OUTPUT_DIR, 'dogs'))
+    # print()
+    #
+    # print('Comparing ImageNet vs Car Models feature visualizations')
+    # run(gram_distance_fn, IMAGENET_DIR, CAR_DIR, os.path.join(OUTPUT_DIR, 'cars'))
+
+    cosine_sim_resnet = CosineSimResnet50()
+    cosine_sim_resnet = cosine_sim_resnet.to(DEVICE)
 
     print('Comparing ImageNet vs Dog Breeds feature visualizations')
-    run(gram_distance_fn, IMAGENET_DIR, DOG_DIR, os.path.join(OUTPUT_DIR, 'dogs'))
+    run_cosine_sim(cosine_sim_resnet, IMAGENET_DIR, DOG_DIR, os.path.join(OUTPUT_DIR, 'dogs'))
     print()
 
     print('Comparing ImageNet vs Car Models feature visualizations')
-    run(gram_distance_fn, IMAGENET_DIR, CAR_DIR, os.path.join(OUTPUT_DIR, 'cars'))
+    run_cosine_sim(cosine_sim_resnet, IMAGENET_DIR, CAR_DIR, os.path.join(OUTPUT_DIR, 'cars'))
