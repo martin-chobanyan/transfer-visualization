e659e4c66064c018b1b80ce1d1b54775109f3bd8
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-06-15 23:46:44 -0400

    added install requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ setup(
     name='feature_vis',
     version='0.1dev',
     packages=find_packages(),
+    install_requires=[
+        'kornia',
+        'numpy',
+        'torch',
+        'torchvision',
+        'tqdm'
+    ]
 )
